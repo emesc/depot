d429e0f6b0e20fe11e593be0ba4ade9e4a03f3e5
emesc <tahder77@gmail.com>
2017-01-10 16:47:46 +1100

    Added CRUD actions and views for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,54 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully created"
+      redirect_to products_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    set_product
   end
 
   def edit
+    set_product
   end
+
+  def update
+    set_product
+    if @product.update_attributes(product_params)
+      flash[:notice] = "Product successfully updated"
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    set_product
+    @product.destroy
+    flash[:notice] = "Product successfully deleted"
+    redirect_to products_path
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/new'
-
-  get 'products/show'
-
-  get 'products/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
 end
