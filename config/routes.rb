58e93b089fd0cbaea304fa768509187c0fc6bfeb
emesc <tahder77@gmail.com>
2017-01-10 15:33:37 +1100

    Added products controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/show'
+
+  get 'products/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
